538222ba4af5078cb7d7fa7ba07cccc6f6b63eea
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2020-01-25 12:39:49 +0330

    adding feed, sitemap and google analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -41,9 +41,64 @@ module.exports = {
           }
         }
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-156934757-1',
+      },
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+      },
+    },
+    {
+      use: 'gridsome-plugin-feed',
+      options: {
+        // Required: array of `GraphQL` type names you wish to include
+        contentTypes: ['Post', 'Build'],
+        // Optional: any properties you wish to set for `Feed()` constructor
+        // See https://www.npmjs.com/package/feed#example for available properties
+        feedOptions: {
+          title: 'hessam khoobkar Feed',
+          description: 'My experiences, opinions, and journal.'
+        },
+        // === All options after this point show their default values ===
+        // Optional; opt into which feeds you wish to generate, and set their output path
+        rss: {
+          enabled: true,
+          output: '/feed.xml'
+        },
+        atom: {
+          enabled: false,
+          output: '/feed.atom'
+        },
+        json: {
+          enabled: false,
+          output: '/feed.json'
+        },
+        // Optional: an array of properties passed to `Feed.addItem()` that will be parsed for
+        // URLs in HTML (ensures that URLs are full `http` URLs rather than site-relative).
+        // To disable this functionality, set to `null`.
+        htmlFields: null,
+        // Optional: if you wish to enforce trailing slashes for site URLs
+        enforceTrailingSlashes: false,
+        // Optional: a method that accepts a node and returns true (include) or false (exclude)
+        // Example: only past-dated nodes: `filterNodes: (node) => node.date <= new Date()`
+        filterNodes: (node) => true,
+        // Optional: a method that accepts a node and returns an object for `Feed.addItem()`
+        // See https://www.npmjs.com/package/feed#example for available properties
+        // NOTE: `date` field MUST be a Javascript `Date` object
+        nodeToFeedItem: (node) => ({
+          title: node.title,
+          date: node.date || node.fields.date,
+          content: node.content
+        })
+      }
     }
   ],
-
   transformers: {
     //Add markdown support to all file-system sources
     remark: {
